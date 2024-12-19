aa67cd2b3979dbb270bda2a3533069f0369bc532
Miles Royer <130029923+MilesRoyerr@users.noreply.github.com>
2024-12-18 19:32:49 -0600

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;   
-    public float jumpForce = 5f;   // Jump force
+    public float jumpForce = 5f;  
     public float lookSpeed = 2f;   // Mouse look speed
     public Transform playerCamera; // Reference to the camera attached to the player
 
